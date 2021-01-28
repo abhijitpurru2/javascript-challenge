23f5f083c093d84bb688108d01ea4fceeb0128c9
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-28 13:49:24 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+function displayTable(data){
+    tbody.text("");
+    data.forEach(function (info) {
+        var row = tbody.append("tr");
+        Object.entries(info).forEach(([key, value]) => {
+        var table = row.append("td").text(value);
+        });
+    })
+}
+
+displayTable(tableData);
